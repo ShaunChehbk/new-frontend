a0bd2de0dfe3634c9f2ac0d2a68fc69ff817a6d8
qcloud <ubuntu@localhost.localdomain>
2023-12-13 12:04:19 +0800

    添加Login组件

---
+++ src/components/Login.js
@@ -0,0 +1,84 @@
+import { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "../context/AuthProvider"
+import axios from "../api/axios";
+import Endpoint from "../api/api";
+
+const Login = () => {
+    const { setAuth } = useContext(AuthContext);
+
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errmsg, setErrmsg] = useState("");
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        setErrmsg("");
+    }, [username, password]);
+
+    const loginSubmit = async (e) => {
+        e.preventDefault();
+        console.log(Endpoint.login);
+        try {
+            const response = await axios.post(
+                Endpoint.login,
+                JSON.stringify({ username, password }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                }
+            );
+            console.log(JSON.stringify(response?.data));
+            const accessToken = response?.data?.access;
+            const refreshToken = response?.data?.refresh;
+            setAuth({ username, password, accessToken });
+            setUsername("");
+            setPassword("");
+            setSuccess(true);
+            localStorage.setItem(
+                "refreshToken",
+                JSON.stringify(refreshToken)
+            );
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <>
+            {
+                success ? (
+                    <section>
+                        <h1>Logged</h1>
+                    </section>
+                ) : (
+                    <section>
+                        <form onSubmit={ loginSubmit }>
+                            <label htmlFor="username">Username::</label>
+                            <input
+                                type="text"
+                                id="username"
+                                autoComplete="off"
+                                onChange={( e ) => setUsername( e.target.value )}
+                                value={ username }
+                                required
+                            />
+                            <br/>
+                            <label htmlFor="password">Password::</label>
+                            <input
+                                type="password"
+                                id="password"
+                                onChange={( e ) => setPassword( e.target.value )}
+                                value={ password }
+                                required
+                            />
+                            <br/>
+                            <button>Sign In</button>
+                        </form>
+                    </section>
+                )
+            }
+        </>
+    )
+}
+
+export default Login;
