cae27ae79c98fb53c65d8b77a0570e14f092bc5d
qcloud <ubuntu@localhost.localdomain>
2023-12-13 11:20:20 +0800

    添加Route
    <BrowserRouter>
      <Routes>
        <Route />
        <Route />
      </Routes>
    </BrowserRouter>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { AuthProvider } from './context/AuthProvider';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Login from './components/Login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path='/*' element={<Login />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
