418170a7cb32f740c5f35b0b70996cbcec3aecde
qcloud <ubuntu@localhost.localdomain>
2023-12-13 14:27:04 +0800

    添加刷新token的url

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,9 @@
  * 为什么不用JSON呢？因为JSON不支持注释，以及无法进行变量拼接
  */
 class Endpoint {
-    static base_url = "http://114.132.88.206:8000/"
-    static login = "api/token/"
+    static base_url = "http://114.132.88.206:8000/";
+    static login = "api/token/";
+    static refresh_token = "api/token/refresh";
 }
 
 export default Endpoint;
