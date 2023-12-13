7bbd6764e0895fc9a038e09735b98a5966fe292b
qcloud <ubuntu@localhost.localdomain>
2023-12-13 11:55:51 +0800

    改正了useAuth()将useContext写成useAuth的错误 这会导致RequireAuth在调用useAuth()的时候出现循环调用，出现Maximum call stack exceeded

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import AuthContext from "../context/AuthProvider";
 
 const useAuth = () => {
-    return useAuth(AuthContext);
+    return useContext(AuthContext);
 }
 
 export default useAuth;
