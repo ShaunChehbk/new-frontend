284f8ad8756291e8a0e2ed03cb5e5c5bbde576e6
Ubuntu Server <hack@shauncheh.com>
2024-03-22 14:23:56 +0800

    使用bookmark.css属性，以及解析新的带checkins信息的bookmark数据

--- src/components/app-bookmark/bookmarklist.js
+++ src/components/app-bookmark/bookmarklist.js
@@ -3,12 +3,15 @@ import useAuth from "../../hooks/useAuth";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import { useNavigate } from "react-router-dom";
 import Endpoint from "../../api/api";
+import "./bookmark.css"
+
 const BookmarkList = () => {
     const { auth } = useAuth();
     const [success, setSuccess] = useState(false);
     const [bookmarks, setBookmarks] = useState([]);
     const axiosPrivate = useAxiosPrivate();
     const navigate = useNavigate();
+    const [response, setResponse] = useState("")
 
     useEffect(() => {
         const getBookmarks = async () => {
@@ -16,36 +19,48 @@ const BookmarkList = () => {
                 const response = await axiosPrivate.get(Endpoint.get_bookmarks);
                 setBookmarks(response.data.reverse());
                 setSuccess(true);
+                setResponse("Success");
             } catch (err) {
                 console.log(err);
+                setResponse("Fail")
             }
         }
         if (auth?.accessToken) {
             getBookmarks();
         }
-    }, [auth]);
-    
-    useEffect(() => {
-        console.log(bookmarks);
-    }, [bookmarks]);
+    }, [auth]); 
 
     return (
         <>
-        <div>
-            BookmarkList
-        </div>
-        <div>
-            {bookmarks.map((bookmark, pos) => {
-                // 在{}记得return 
-                return (
-                    <div key={pos}>
-                        {bookmark.title}
-                    </div>
-                )
-            })}
-        </div>
+            <div>
+                BookmarkList
+            </div>
+            <div>
+                {response}
+                {bookmarks.length}
+            </div>
+            <div>
+                {bookmarks.map((bookmark, idx) => {
+                    // 在{}记得return 
+                    return <Bookmark 
+                        bookmark={bookmark}
+                        idx={idx}
+                    />
+                })}
+            </div>
         </>
     )
 };
+
+const Bookmark = ({bookmark, idx}) => {
+    return (
+        <div className="bookmark-title" key={idx}>
+            <a href={bookmark.url}>
+                {bookmark.title}
+                {bookmark.checkins.length}
+            </a>
+        </div>
+    ) 
+}
 
 export default BookmarkList;
