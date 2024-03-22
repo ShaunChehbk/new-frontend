e005727dd65ef295a5006b28f7de0788086230bc
Ubuntu Server <hack@shauncheh.com>
2024-03-22 11:30:34 +0800

    在<Home />中添加<Route />条目

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 import RequireAuth from "./components/RequireAuth";
+import PersistLogin from "./components/PersistLogin";
+import BookmarkList from "./components/app-bookmark/bookmarklist";
+import Navigation from "./components/Navigation";
+import ProjectMgr from "./components/app-projectmgr/projectmgr";
+import PlayGround from "./components/playground/draw-box";
+import "./Home.css"
+import PlayGround2 from "./components/playground/2";
 
 const Home = () => {
     return (
         <Routes>
-            <Route element={ <RequireAuth /> }>
-                <Route path="/" element={ <LoggedIn /> } />
+            <Route element={<PersistLogin />}>
+            <Route element={<RequireAuth />}>
+                <Route path="/" element={<Navigation />} >
+                    <Route path="Bookmark" element={<BookmarkList />} />
+                    <Route path="ProjectMgr" element={<ProjectMgr />} />
+                    <Route path="Playground" element={<PlayGround />} />
+                    <Route path="Playground2" element={<PlayGround2 />} />
+                </Route>
             </Route>
-            <Route path="*" element={ <Notfound /> } />
+            </Route>
+            <Route path="*" element={<Notfound />} />
         </Routes>
     )
 }
