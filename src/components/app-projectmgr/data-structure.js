00eb70d8ffa7a0fcc993cfb751180ae57a4a0b97
Ubuntu Server <hack@shauncheh.com>
2024-03-22 11:02:58 +0800

    sample: Work数据结构

---
+++ src/components/app-projectmgr/data-structure.js
@@ -0,0 +1,119 @@
+// Collection？还是Project？
+
+export const WorkList = [
+    {
+        "name": "学习CMake",
+        "tasks": [
+            {
+                "name": "弄清CMake基本语法"
+            }
+        ]
+    },
+    {
+        "name": "iOS share Extension 插件（URL搜集）",
+        "tasks": [
+            {
+                "name": "Swift HTTP request"
+            },
+            {
+                "name": "Share Extension 插件"
+            }
+        ]
+    },
+    {
+        "name": "Chrome插件开发",
+        "tasks": [
+            {
+                "name": "添加右键记录生词"
+            },
+            {
+                "name": "添加右键记录例句"
+            }
+        ]
+    },
+    {
+        "name": "后端功能开发",
+        "task": [
+            {
+                "name":  "touch标签页"
+            }
+        ]
+    },
+    {
+        "name": "雕塑每日练习",
+        // stage or phase
+        "stage": "",
+        "tasks": [
+            {
+                "name": "大形练习"
+            },
+            {
+                "name": "鼻子练习"
+            }
+        ]
+    },
+    {
+        "name": "学习SpeedChar雕塑流程",
+        "tasks": [
+            {
+                "name": "头部大形教程"
+            },
+            {
+                "name": "五官细节教程"
+            },
+            {
+                "name": "躯干教程"
+            }
+        ]
+    },
+    {
+        "name": "html基础知识补全",
+        "tasks": [
+            {
+                "name": "认识基本元素"
+            },
+            {
+                "name": "弄清图形元素的基本原理"
+            }
+        ]
+    },
+    {
+        "name": "Javascript基础知识补全"
+    },
+    {
+        "name": "学习Swift",
+        "tasks": [
+            {
+                "name": "数据类型"
+            },
+            {
+                "name": "控制流"
+            },
+            {
+                "name": "闭包"
+            }
+        ]
+    },
+    {
+        "name": "前端功能开发",
+        "tasks": [
+            {
+                "name": "登录状态保持PersistLogin"
+            }
+        ]
+    },
+    {
+        "name": "绘画",
+        "tasks": [
+            {
+                "name": "学完潘大的课"
+            }
+        ]
+    }
+]
+
+class Work {
+    constructor(name) {
+        this.name = name
+    }
+}
